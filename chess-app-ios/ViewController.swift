f510fca91904b5e427fce3c3cef456cd2433e606
Alex Giatrakis <giatrakisalex@gmail.com>
2023-07-06 16:38:29 +0300

    Initial commit

---
+++ chess-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chess-app-ios
+//
+//  Created by Alex Giatrakis on 6/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
